81cba78c21015dbf8ff0754c4d03c89a950e7acf
Evgeny <evolenko@inbox.ru>
2022-12-01 23:02:42 +0300

    Added Task029

---
+++ Task029HW_Array_UserEnter/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+Console.Clear();
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите значение элемента массива в позиции [{i}]: ");
+    array[i] = EnterDigit();
+}
+
+PrintArray(array);
+
+int EnterDigit()
+{
+    int digit;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out digit)) break;
+        else Console.Write("Ну просил же ЧИСЛО: ");
+    }
+    return digit;
+}
+
+void PrintArray (int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.WriteLine($"Позиция в массиве {i} значение {col[i]}.");
+    }
+}
