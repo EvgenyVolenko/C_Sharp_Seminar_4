8bb56bf297745ac69ce2d5f308f084a5d3490102
Evgeny <evolenko@inbox.ru>
2022-12-01 20:45:15 +0300

    Initial and Task024

---
+++ Task024CW_Sum_1_to_N/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+
+
+Console.Write("Введите положительное число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int index = 1;
+if (a <= 0) Console.WriteLine("Число меньше 0, запустите программу заново.");
+else
+{
+    while (index <= a)
+    {
+        sum += index;
+        index++;
+    }
+    Console.WriteLine($"Сумма чисел от 1 до {a} = {sum}");
+}
+
