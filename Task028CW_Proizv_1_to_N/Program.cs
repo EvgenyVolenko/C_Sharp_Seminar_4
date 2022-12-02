8f7b115ab848f42b87d5311ad20b479bd2954627
EvgenyVolenko <evolenko@inbox.ru>
2022-12-02 16:28:57 +0300

    Correct by Seminar Tasks 028

--- Task028CW_Proizv_1_to_N/Program.cs
+++ Task028CW_Proizv_1_to_N/Program.cs
@@ -3,23 +3,47 @@
 // 5 -> 120
 
 Console.Clear();
-int digit, count = 1, pr = 1;
 
-Console.Write("Введите число A которое больше 0: ");
-
-while (true)
+int number = GetNumber("Введите положительно число N: ");
+bool isCorrect = Validate(number);
+if (isCorrect == true)
 {
-    if (int.TryParse(Console.ReadLine(), out digit)) break;
-    else Console.Write("Ну просил же ЧИСЛО: ");
+    int pr = PrFromDigits(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {pr}");
 }
-
-if (digit <= 0) Console.WriteLine("Ну больше же 0 просил.");
 else
 {
-    while (count < digit)
+    Console.WriteLine($"Ну просил же положительное ЧИСЛО.");
+}
+
+int GetNumber(string message)
+{
+    int digit;
+    Console.Write(message);
+    if (int.TryParse(Console.ReadLine(), out digit)) return digit;
+    else return -1;
+}
+
+int PrFromDigits(int A)
+{
+    int count = 0;
+    int proizv = 1;
+    while (count < A)
     {
         count++;
-        pr *= count;
+        proizv *= count;
     }
-    Console.WriteLine($"Выводим произведение чисел {pr}");
+    return proizv;
+}
+
+bool Validate(int dig)
+{
+    if (dig > 0) 
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
